7413177ad7447c996a4656b85538ccb1e750d572
Akshay Raman <ar10021161@gmail.com>
2023-09-08 20:52:35 -0400

    completed 6.006 problem set - 0

--- 6.006/ps0/ps0-template/count_long_subarray.py
+++ 6.006/ps0/ps0-template/count_long_subarray.py
@@ -3,8 +3,18 @@ def count_long_subarray(A):
     Input:  A     | Python Tuple of positive integers
     Output: count | number of longest increasing subarrays of A
     '''
-    count = 0
-    ##################
-    # YOUR CODE HERE #
-    ##################
+    count = 1
+    length = 1
+    current = 1
+    for i in range(1, len(A)):
+        if A[i-1] < A[i]:
+            current += 1
+        else:
+            current = 1
+            
+        if current == length:
+            count += 1
+        elif current > length:
+            length = current
+            count = 1
     return count
